449bf47c10ca40e37cb475f10420966231a09e40
abhigitit <90536339+abhigitit@users.noreply.github.com>
2022-05-19 10:48:26 -0700

    py file for commit track; Data Cleaning

---
+++ code/01_RandomForest_Xboost_Abhiteja.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### Importing requires libraries
+
+
+
+pip install xgboost
+
+
+
+
+import numpy as np
+import pandas as pd
+import os
+import re
+import nltk
+import string
+import seaborn as sns
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import precision_score, recall_score
+import xgboost as xgb
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# ### Reading csv into pandas dataframe
+
+
+tweets = pd.read_csv('train.csv')
+tweets.head(5)
+
+
+tweets.info()
+
+tweets.shape
+
+
+# #### Location wise values
+
+tweets['location'].value_counts()
+
+# #### Keywords count
+
+tweets['keyword'].value_counts()
+
+
+# ### Data Cleaning
+
+
+cols_to_drop = ['location','id']
+
+tweets = tweets.drop(cols_to_drop, axis = 1)
+
+
+tweets["CleanText"] = tweets["text"].apply(lambda x: x.lower())
+tweets.head()
+
+
+
+tweets["CleanText"] = tweets["CleanText"].apply(lambda x: re.sub(r"https?://\S+|www\.\S+", "",x))
+
+
+def removeHTML(text):
+    html = re.compile(r"<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});")
+    return re.sub(html, "", text)
+
+tweets["CleanText"] = tweets["CleanText"].apply(lambda x: removeHTML(x))
+
+
+def removeEmojis(text):
+    emoji_pattern = re.compile("["
+                       u"\U0001F600-\U0001F64F"  # emoticons
+                       u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                       u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                       u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                       u"\U00002702-\U000027B0"
+                       u"\U000024C2-\U0001F251"
+                       u"\U0001f926-\U0001f937"
+                       u"\u200d"
+                       u"\u2640-\u2642" 
+                       "]+", flags=re.UNICODE)
+    return emoji_pattern.sub(r'', text)
+
+tweets["CleanText"] = tweets["CleanText"].apply(lambda x: removeEmojis(x))
+
+
+def RemovePunctuation(text):
+    return text.translate(str.maketrans('', '', string.punctuation))
+
+tweets["CleanText"] = tweets["CleanText"].apply(lambda x: RemovePunctuation(x))
+
+
+def RemoveASCII(text):
+  return re.sub(r'[^\x00-\x7f]', "", text)
+
+tweets["CleanText"] = tweets["CleanText"].apply(lambda x: RemoveASCII(x))
+
+
+tweets.head(30)
+
+
