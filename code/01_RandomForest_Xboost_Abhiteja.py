57cba0592134273a7187862717cf61f9c7dd4396
abhigitit <90536339+abhigitit@users.noreply.github.com>
2022-05-19 10:51:38 -0700

    Preliminary Analysis update

--- code/01_RandomForest_Xboost_Abhiteja.py
+++ code/01_RandomForest_Xboost_Abhiteja.py
@@ -108,7 +108,36 @@ features = ['keyword']
 for feat in features : 
     print("The number of missing values in "+ str(feat)+" are "+str(tweets[feat].isnull().sum()))
     
+# ### Analysis
+
+
+plot_size = plt.rcParams["figure.figsize"]
+print(plot_size[0])
+print(plot_size[1])
+
+plot_size[0]=8
+plot_size[1]=6
+plt.rcParams["figure.figsize"]=plot_size
+
+
+# ##### target = 1 is when disaster occurs and target = 0 is when disaster does not occur
+
+# In[21]:
+
+
+x=tweets.target.value_counts()
+sns.barplot(x.index,x)
+plt.gca().set_ylabel('samples')
+
+
+
+
+def _corpus(target):
+    corpus=[] # document
     
-    
+    for x in tweets[tweets['target']==target]['text'].str.split():
+        for i in x:
+            corpus.append(i)
+    return corpus
 
 
